a7d4c83c7238a2541009b987466415f22e4686d9
Louie Bao <louie.bao@gmail.com>
2020-01-11 15:27:12 +1100

    Update Check-Code-Consistency.ps1

--- powershell/Check-Code-Consistency.ps1
+++ powershell/Check-Code-Consistency.ps1
@@ -92,3 +92,106 @@ foreach ($object_path in $object_path_array)
         }
     }   
 }
+
+##################
+# Check nvarchar #
+##################
+Write-Host "`r`nString constants should be prefixed with N to make them nvarchar:"  -ForegroundColor Cyan
+
+foreach ($object_path in $object_path_array)
+{
+    $files = Get-ChildItem $object_path -Recurse
+
+    foreach ($file in $files)
+    {
+        if ($file.FullName | Select-String -Pattern "\Functions\", "\Stored Procedures\", "\Tables\", "\Views" -SimpleMatch -Quiet)
+        {
+            if (Select-String -Path $file.FullName -Pattern "=\s*'[^']" -Quiet)
+            {
+                "$($file.Name)"
+            }
+        }
+    }
+}
+
+#######
+# xxx #
+#######
+Write-Host "`r`nDatabase context [xxx] should not be hardcoded in the current database:" -ForegroundColor Cyan
+foreach ($object_path in $object_path_array)
+{
+    if ($object_path | Select-String -Pattern "xxx" -SimpleMatch -Quiet)
+    {
+        Select-String $object_path -Pattern "xxx." -SimpleMatch
+    }
+}
+
+###########################################
+# Check dbo. in front of table/proc names #
+###########################################
+Write-Host "`r`nMissing dbo. in front table/proc names:" -ForegroundColor Cyan
+
+foreach ($object_path in $object_path_array)
+{
+    Select-String $object_path -Pattern "from t_", "join t_" -SimpleMatch
+    Select-String $object_path -Pattern "exec p_" -SimpleMatch
+    Select-String $object_path -Pattern "execute p_" -SimpleMatch
+}
+
+############################################
+# Format numbers to #,##0 instead of #,### #
+############################################
+Write-Host "`r`nFormat numbers to #,##0 instead of #,###:" -ForegroundColor Cyan
+
+$procs = Get-ChildItem "C:\dev\project1\Stored Procedures\*.sql"
+
+foreach ($proc in $procs)
+{
+    if (Select-String -Path $proc.FullName -Pattern "#,###" -SimpleMatch -Quiet)
+    {
+        $proc.BaseName
+    }
+}
+
+##########################
+# Detected ALTER PROC in #
+##########################
+Write-Host "`r`nProc DML needs to start with CREATE instead of ALTER:" -ForegroundColor Cyan
+
+$procs = Get-ChildItem "C:\dev\project1\Stored Procedures\*.sql"
+
+foreach ($proc in $procs)
+{
+    if (Select-String -Path $proc.FullName -Pattern "alter proc" -SimpleMatch -Quiet)
+    {
+        $proc.BaseName
+    }
+}
+
+##############################################################
+# LHS should not be the same as RHS in an equality condition #
+##############################################################
+Write-Host "`r`nLHS should not be the same as RHS in an equality condition:" -ForegroundColor Cyan
+
+$procs = Get-ChildItem "C:\dev\project1\Stored Procedures\*.sql"
+
+foreach ($proc in $procs)
+{
+    $match_info = Select-String -Path $proc.FullName -Pattern "\s*[^\s\)]+\s*=\s*[^\s\)]+" -AllMatches
+
+    foreach ($match in $match_info.Matches) {
+
+        $lhs, $rhs = $match.value.Split("=")
+
+        $lhs = $lhs.Trim()
+        $rhs = $rhs.Trim()
+
+        if ((!$lhs.StartsWith("@")) -and (!$rhs.StartsWith("@")) `
+            -and (!($lhs -eq 1)) -and (!($rhs -eq 1))) {
+            if ($lhs -eq $rhs) {
+
+                "$($proc.BaseName): $($match.value)"
+            }
+        }
+    }
+}
